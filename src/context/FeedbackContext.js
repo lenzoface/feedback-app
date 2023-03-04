6fd1e4ff69cef879637f3a98d0880dd4b9eef4d3
Sherkhan K <sherkhan4work@gmail.com>
2023-03-05 01:37:42 +0600

    add,update,delete on backend

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -19,7 +19,8 @@ export const FeedbackProvider = ({children}) => {
 
     // Fetch feedback
     const fetchFeedback = async () => {
-        const response = await fetch(`http://localhost:5000/feedback?_sort=id&_order=desc`)
+        // added proxy in package.json, so no need to pu it here before 'feedback' (http://localhost:5000/)
+        const response = await fetch(`feedback?_sort=id&_order=desc`)
         const data = await response.json()
 
         setFeedback(data)
@@ -27,21 +28,43 @@ export const FeedbackProvider = ({children}) => {
     }
 
     // Add feedback
-    const addFeedback = (newFeedback) => {
-        newFeedback.id = uuidv4()
-        setFeedback([newFeedback, ...feedback])
+    const addFeedback = async (newFeedback) => {
+        const response = await fetch('/feedback', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(newFeedback),
+        })
+
+        const data = await response.json()
+
+        // newFeedback.id = uuidv4()  //DONT HAVE TO NO MORE WITH JSON SERVER
+        setFeedback([data, ...feedback])
     }
 
     // Delete feedback
-    const deleteFeedback = (id) => {
+    const deleteFeedback = async (id) => {
         if (window.confirm('Are you sure you want to delete?')) {
+            await fetch(`/feedback/${id}`, { method: 'DELETE'})
+
             setFeedback(feedback.filter((item) => item.id !== id))
         }
     }
 
     // Update feedback item
-    const updateFeedback = (id, updItem) => {
-        setFeedback(feedback.map((item) => item.id === id ? {...item, ...updItem} : item))
+    const updateFeedback = async (id, updItem) => {
+        const response = await fetch(`/feedback/${id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(updItem)
+        })
+
+        const data = await response.json()
+
+        setFeedback(feedback.map((item) => item.id === id ? {...item, ...data} : item))
     }
 
     // Set item to be updated | function
